2e21c6f57cb71fb7a92c693a29d60ae2a9300cc0
blink992 <alleybossmangas@gmail.com>
2025-07-10 04:22:49 -0300

    att

--- src/ConnectDB.py
+++ src/ConnectDB.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
-from models import Base
+from src.models import Base
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
