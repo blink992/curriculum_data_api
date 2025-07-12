952a3adbc1c73ad7d750008e48294ac303ad8516
blink992 <alleybossmangas@gmail.com>
2025-07-11 21:25:55 -0300

    atualizacao no gitignore e adicao do get para checar o status da api

--- main.py
+++ main.py
@@ -7,17 +7,22 @@ from models.models import *
 from schemas.models import *
 
 @app.get("/")
-def root():
-    return {"mensagem": "API no ar!"}
+async def root():
+    return {"mensagem": "curriculum_data_api no ar!"}
+
+@app.get("/health")
+async def health_status():
+    return {"mensagem": "curriculum_data_api no ar!"}
+
 
 @app.get("/pt/about_me", response_model=people_out)
-def get_about_me_pt(people_id: int = 1, db: Session = Depends(get_db)):
+async def get_about_me_pt(people_id: int = 1, db: Session = Depends(get_db)):
     return db.query(people).filter(people.id == people_id).first()
 
 @app.get("/pt/get_people_id", response_model=people_out_id)
-def get_people_id(people_name: str, db: Session = Depends(get_db)):
+async def get_people_id(people_name: str, db: Session = Depends(get_db)):
     return db.query(people).filter(people.name == people_name).first()
 
 @app.get("/pt/get_academic_training", response_model=List[academic_training_out])
-def get_academic_training(people_id: int, db: Session = Depends(get_db)):
+async def get_academic_training(people_id: int, db: Session = Depends(get_db)):
     return db.query(academic_training).filter(academic_training.people_id == people_id).all()
