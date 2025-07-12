a3a0222679a2341051150c2a7e9e5a2af4fbb9e6
blink992 <alleybossmangas@gmail.com>
2025-07-11 21:34:15 -0300

    mudança no path dos gets

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ async def health_status():
     return {"mensagem": "curriculum_data_api no ar!"}
 
 
-@app.get("/pt/about_me", response_model=people_out)
+@app.get("/get/about_me", response_model=people_out)
 async def get_about_me_pt(people_id: int = 1, db: Session = Depends(get_db)):
     return db.query(people).filter(people.id == people_id).first()
 
-@app.get("/pt/get_people_id", response_model=people_out_id)
+@app.get("/get/people_id", response_model=people_out_id)
 async def get_people_id(people_name: str, db: Session = Depends(get_db)):
     return db.query(people).filter(people.name == people_name).first()
 
-@app.get("/pt/get_academic_training", response_model=List[academic_training_out])
+@app.get("/get/academic_training", response_model=List[academic_training_out])
 async def get_academic_training(people_id: int, db: Session = Depends(get_db)):
     return db.query(academic_training).filter(academic_training.people_id == people_id).all()
